b2a7053d5d2b1ddd7d95dc8f403ae073d573f7a2
“Iuliia” <ybe2004ybe@gmail.com>
2026-02-04 17:22:58 +0300

    Initial Commit

---
+++ bills-subscriptions/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bills-subscriptions
+//
+//  Created by Iuliia Emelianova on 4.2.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bills-subscriptions/bills_subscriptionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  bills_subscriptionsApp.swift
+//  bills-subscriptions
+//
+//  Created by Iuliia Emelianova on 4.2.2026.
+//
+
+import SwiftUI
+
+@main
+struct bills_subscriptionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
